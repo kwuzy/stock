ac78b6d4edf97141f42b7c2512216add41a8cb7a
Kevin Wu <kevin.wu@iherb.com>
2020-02-06 11:30:54 -0800

    ticker and button

---
+++ stockdownload-app/src/components/Stock.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+
+
+export default function Stock() {
+    const [ stock, setStock ] = useState(null);
+
+    function handleStockChange(event) {
+        setStock(event.target.value);
+        console.log(stock);
+    }
+
+    function handleSearch(event) {
+        console.log(`Stock is: ${stock}`);
+    }
+
+    return (
+        <div>
+            <div className="stock-input">
+                <Input placeholder="Ticker Symbol" onChange={handleStockChange} />
+                <Button onClick={handleSearch} color="primary" variant="outlined">Load</Button>
+            </div>
+        </div>
+    )
+}
