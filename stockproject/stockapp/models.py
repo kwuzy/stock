9fde4f6329b0b47b771bfbd051b3edff070189cb
Kevin Wu <kevin.wu@iherb.com>
2020-02-18 09:10:58 -0800

    fixed the name change and created model

--- stockproject/stockapp/models.py
+++ stockproject/stockapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Stocks(models.Model):
+    ticker = models.CharField(max_length = 100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
